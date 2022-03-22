7e3d9af032d91ab171e3e6e6a04bea86c21a4a9a
Benjamin Arnold <chillstepcoder@gmail.com>
2022-03-22 10:36:29 -0700

    Fix anim import p1

--- RTS/src/resources/AnimationRepository.cpp
+++ RTS/src/resources/AnimationRepository.cpp
@@ -16,21 +16,24 @@ AnimationRepository::~AnimationRepository() {
 
 bool AnimationRepository::loadAnimFile(const vio::Path& filePath) {
     std::ifstream ifstr(filePath.getCString(), std::ifstream::in);
-
+    vio::Path rootDir = filePath;
+    rootDir.trimEnd();
     constexpr int BUFFER_SIZE = 256;
     char buffer[BUFFER_SIZE];
     while (ifstr.getline(buffer, BUFFER_SIZE)) {
-        vio::Path animPath = filePath.getString() + "\\" + buffer;
-        ozz::io::File file(animPath.getCString(), "rb");
+        vio::Path animPath = rootDir.getString() + "\\" + buffer;
+        vio::Path absolutePath;
+        assert(mIoManager.resolvePath(animPath, absolutePath));
+        ozz::io::File file(absolutePath.getCString(), "rb");
 
         if (!file.opened()) {
-            pError("Animation import failure - " + animPath.getString());
+            pError("Animation import failure - " + absolutePath.getString());
             assert(false);
         }
 
         ozz::io::IArchive archive(&file);
         if (!archive.TestTag<ozz::animation::Animation>()) {
-            pError("Animation file is not an animation - " + animPath.getString());
+            pError("Animation file is not an animation - " + absolutePath.getString());
             assert(false);
         }
         ozz::animation::Animation& anim = mAnimations.emplace_back();
